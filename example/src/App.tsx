623a09f5794d021db10fddf9cae3aa84b6d52991
nick bouldien <nick.bouldien@gmail.com>
2019-07-18 22:17:59 -0500

    link the library to the example and use in App.tsx to verify

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import { getRandomNumber } from "react-hall-pass";
 
 const App: React.FC = () => {
+  const num = getRandomNumber();
   return (
     <div className="App">
       <h1>React Hall Pass example</h1>
+      <p>number: {num}</p>
     </div>
   );
 };
