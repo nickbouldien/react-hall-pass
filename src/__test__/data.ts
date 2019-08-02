4c8b0fb70b59515b75fe9122715008cde733b371
nick bouldien <nick.bouldien@gmail.com>
2019-08-01 23:22:45 -0500

    test: add tests for useHallPass and make file to hold test data

---
+++ src/__test__/data.ts
@@ -0,0 +1,28 @@
+export type User = {
+  name: string;
+  permissions: string[];
+};
+
+export interface Users {
+  [s: string]: User;
+}
+
+export const fan1: User = {
+  name: "josh",
+  permissions: []
+};
+
+export const employee1 = {
+  name: "rafael",
+  permissions: ["PAY_PLAYER"]
+};
+
+export const employee2 = {
+  name: "dave",
+  permissions: ["PAY_PLAYER", "SCHEDULE_GAME"]
+};
+
+export const employee3 = {
+  name: "jorge",
+  permissions: ["PAY_PLAYER", "SCHEDULE_GAME", "DRAFT_PLAYER"]
+};
---
+++ src/__test__/useHallPass.test.ts
@@ -0,0 +1,65 @@
+import useHallPass from "../useHallPass";
+import { fan1, employee1, employee3 } from "./data";
+
+const emptyRequiredPermissions: string[] = [];
+
+const requiredPermissions1 = ["PAY_PLAYER"];
+
+const requiredPermissions2 = ["DRAFT_PLAYER"];
+
+const requiredPermissions3 = ["PAY_PLAYER", "SCHEDULE_GAME"];
+
+const requiredPermissions4 = ["PAY_PLAYER", "SCHEDULE_GAME", "DRAFT_PLAYER"];
+
+/* empty requiredPermissions */
+test("useHallPass returns true for empty array of requiredPermissions - employee", () => {
+  const result = useHallPass(employee1.permissions, emptyRequiredPermissions);
+  expect(result).toBe(true);
+});
+
+test("useHallPass returns true for empty array of requiredPermissions - fan", () => {
+  const result = useHallPass(fan1.permissions, emptyRequiredPermissions);
+  expect(result).toBe(true);
+});
+
+/* single requiredPermission */
+test("useHallPass returns false for array of one requiredPermission that the user does not have", () => {
+  const result = useHallPass(employee1.permissions, requiredPermissions2);
+  expect(result).toBe(false);
+});
+
+test("useHallPass returns true for array of one requiredPermission that the user does have - 1", () => {
+  const result = useHallPass(employee1.permissions, requiredPermissions1);
+  expect(result).toBe(true);
+});
+
+test("useHallPass returns true for array of one requiredPermission that the user does have - 2", () => {
+  const result = useHallPass(employee3.permissions, requiredPermissions1);
+  expect(result).toBe(true);
+});
+
+test("useHallPass returns true for array of one requiredPermission that the user does have - 3", () => {
+  const result = useHallPass(employee3.permissions, requiredPermissions2);
+  expect(result).toBe(true);
+});
+
+/* multiple requiredPermissions */
+test("useHallPass returns false for array of multiple requiredPermission that the user does not have - 1", () => {
+  const result = useHallPass(employee1.permissions, requiredPermissions3);
+  expect(result).toBe(false);
+});
+
+test("useHallPass returns false for array of multiple requiredPermission that the user does not have - 2", () => {
+  const result = useHallPass(employee1.permissions, requiredPermissions4);
+  expect(result).toBe(false);
+});
+
+test("useHallPass returns true for array of multiple requiredPermission that the user does have - 1", () => {
+  const result = useHallPass(employee3.permissions, requiredPermissions3);
+  expect(result).toBe(true);
+});
+
+test("useHallPass returns true for array of multiple requiredPermission that the user does have - 2", () => {
+  const result = useHallPass(employee3.permissions, requiredPermissions4);
+  expect(result).toBe(true);
+});
