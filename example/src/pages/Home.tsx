7fe0f1eee5b803ed61a69eb22575d4a4c947495a
nick bouldien <nick.bouldien@gmail.com>
2019-08-25 16:56:54 -0500

    docs: add links to the home page (temporary solution)

--- example/src/pages/Home.tsx
+++ example/src/pages/Home.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const Home: React.FC = () => {
   return (
     <div className="home">
       <h1>React Hall Pass example</h1>
+      <ul>
+        <li>
+          <Link to="example">example using arrays of permissions</Link>
+        </li>
+
+        <li>
+          <Link to="example-strings">example using permission strings</Link>
+        </li>
+
+        <li>
+          <Link to="all-permissions">all permissions</Link>
+        </li>
+
+        <li>
+          <Link to="helper-components">an example of a "helper component"</Link>
+        </li>
+
+        <li>
+          <Link to="hook">hook example</Link>
+        </li>
+      </ul>
     </div>
   );
 };
